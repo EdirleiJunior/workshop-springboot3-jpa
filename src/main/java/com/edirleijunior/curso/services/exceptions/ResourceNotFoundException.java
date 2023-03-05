2bd782f525eb652c04a4a1faffc919290d34984e
Edirlei Junior <edirleijunior89@gmail.com>
2023-03-05 16:40:28 -0300

    Exception handling - delete

--- src/main/java/com/edirleijunior/curso/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/edirleijunior/curso/services/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,8 @@
 package com.edirleijunior.curso.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-		private static final long serialVersionUID = 1L;
+	
+	private static final long serialVersionUID = 1L;
 		
 	public ResourceNotFoundException(Object id) {
 		super("Resource not found. Id " + id);
